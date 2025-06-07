1201fba0965a8d36353ee5ab5819a9787d11700d
pranav5127 <pranav.kumar.5127@gmail.com>
2025-06-07 22:28:56 +0530

    remove unused imports

--- app/src/main/java/com/sugardevs/flashcards/data/network/api/ApiService.kt
+++ app/src/main/java/com/sugardevs/flashcards/data/network/api/ApiService.kt
@@ -3,10 +3,8 @@ package com.sugardevs.flashcards.data.network.api
 import com.sugardevs.flashcards.data.network.model.CardsRequest
 import com.sugardevs.flashcards.data.network.model.CardsResponse
 import okhttp3.MultipartBody
-import okhttp3.Request
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
