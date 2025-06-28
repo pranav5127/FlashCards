72fdd47a5dd799ad0acfa965a2b5667a17787f0b
pranav5127 <pranav.kumar.5127@gmail.com>
2025-06-28 23:10:51 +0530

    code clean up

--- app/src/main/java/com/sugardevs/flashcards/navigation/Navigation.kt
+++ app/src/main/java/com/sugardevs/flashcards/navigation/Navigation.kt
@@ -1,26 +1,12 @@
 package com.sugardevs.flashcards.navigation
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.sugardevs.flashcards.ui.components.BottomBar
-import com.sugardevs.flashcards.ui.components.SearchBar
-import com.sugardevs.flashcards.ui.components.TopBar
-import com.sugardevs.flashcards.ui.viewModels.CardsScreenViewModel
 import com.sugardevs.flashcards.ui.viewModels.auth.AuthViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
